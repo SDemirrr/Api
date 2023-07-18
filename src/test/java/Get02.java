c2932da338309493513abf8d8902f5cf1f866f1b
SDemirrr <101672380+SDemirrr@users.noreply.github.com>
2023-07-18 23:55:27 +0300

    18-7-23

---
+++ src/test/java/Get02.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class Get02 {
+
+    //i)Set the URL
+    //ii) Set the expected Data
+    //iii) Type code to send request
+    //iv) Do Assertion
+
+    @Test
+    public void get02(){
+
+    }
+}
